31f86a5189213795ed2623e2573430289053dd96
Viraj kanjia <virajkanjia9@gmail.com>
2023-11-23 11:32:37 +0530

    Create two utils file to handle error and async function call

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path: './.env'
 })
-connectDB();
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server running aty port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log('mongo connection fail ', err);
+})
 
 
 // Another way to connect database
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+    Promise .resolve(requestHandler(req,res,next))
+            .catch(next(err))
+}
+}
+
+export {asyncHandler}
+
+// ------ another way ------------
+
+// const asyncHandler = () => {}
+// const asyncHandler = () => {() => {}}
+// const asyncHandler = () => () => {}
+
+// const asyncHandler = (func) => async (req,res,next) => {
+//     try {
+//         await func(req,res,next)
+//     } catch (error) {
+//         res.status(error.code || 500)
+//             .json({
+//                 success: false,
+//                 message: error.message
+//             })
+//     }
+// }
