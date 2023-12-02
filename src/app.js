1107c9a281f86c32d611d5e7032702695fc84a0c
Viraj kanjia <virajkanjia9@gmail.com>
2023-12-02 19:05:41 +0530

    modify app.js and asyncHandler file also add user controller and route

--- src/app.js
+++ src/app.js
@@ -12,4 +12,12 @@ app.use(express.json({limit:'16kb'}))
 app.use(express.urlencoded({extended:true, limit:'16kb'}))
 app.use(cookieParser())
 
+
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+
+//route declaration
+app.use('/api/v1/user',userRouter)
+
 export {app}
